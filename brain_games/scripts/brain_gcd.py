17375e0cfd8766bce633daa098e2806689c6b020
Виктория Мальцева <malvicktoria2603@gmail.com>
2023-10-31 20:12:20 +0700

    Correct brain_gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,8 +3,8 @@ from brain_games.engine import run
 
 
 def main():
-    module = __import__('brain_games.scripts.games.brain_even',
-            fromlist=['brain_even'])
+    module = __import__('brain_games.scripts.games.brain_gcd',
+            fromlist=['brain_gcd'])
     run(module)
 
 
